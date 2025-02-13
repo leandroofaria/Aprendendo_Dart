bcb0eee9ec10b9eaf722720de73dc958b3b08b32
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-02-13 14:31:13 -0300

    MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(AppWidget(title: 'Testando',));
+  runApp(AppWidget(title: 'Testando'));
 }
 
 class AppWidget extends StatelessWidget {
   final String title;
 
   const AppWidget({super.key, required this.title});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black, // Definindo a cor de fundo do Container
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 20.0),
-        ),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Container(
+        child: Center(
+          child: Text('testando')
+          ),
       ),
     );
   }
