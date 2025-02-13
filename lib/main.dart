594e6dc097d5624cd65cb005e8af0be13fc55220
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-02-13 14:20:33 -0300

    Estados

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.black,  // Definindo a cor de fundo do Container
-    child: Center(
-      child: Text(
-        'Testando',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 20.0),
+  runApp(AppWidget(title: 'Testando',));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black, // Definindo a cor de fundo do Container
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 20.0),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
